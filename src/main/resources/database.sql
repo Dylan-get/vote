81211fac372b8ad448d8b6df35c3afdb198f02d5
WuHaitao <1791360085@qq.com>
2021-02-24 22:39:52 +0800

    create

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,15 +1,16 @@
 CREATE TABLE user(
     userId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     userName varchar(255) NOT NULL,
-    passwd varchar(255) NOT NULL,
-);
+    passwd varchar(255) NOT NULL
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
 CREATE TABLE votelist(
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    voteId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     voteName varchar(255) NOT NULL,
-    userId varchar(255) NOT NULL,
-);
+    userId varchar(255) NOT NULL
+)ENGINE=INNODB DEFAULT CHARSET=utf8;
 CREATE TABLE choicelist(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    voteId int NOT NULL,
     choiceName varchar(255) NOT NULL,
-    passwd varchar(255) NOT NULL,
-);
+    num int NOT NULL
+)ENGINE=INNODB DEFAULT CHARSET=utf8;
